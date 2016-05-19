8d404c9e769cb500c1546e4a921ea34e9161151b
Andrew Jesaitis <andrew@andrewjesaitis.com>
2016-05-19 14:35:31 -0600

    Adds navbar

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,12 +1,33 @@
 var React = require('react');
 
+function Navbar() {
+  return (
+    <nav className="navbar navbar-default">
+      <div className="container-fluid">
+        <div className="navbar-header">
+          <a className="navbar-brand" href="#">Brand</a>
+        </div>
+        <form className="navbar-form navbar-right" role="search">
+          <div className="form-group">
+            <input type="text" className="form-control" placeholder="Location" />
+          </div>
+          <button type="submit" className="btn btn-default">Submit</button>
+        </form>
+      </div>
+    </nav>
+  );
+}
+
 var Main = React.createClass({
   propTypes: {
     children: React.PropTypes.element.isRequired,
   },
   render() {
     return (
-      <div className="main-container">{this.props.children}</div>
+      <div classNameName="main-container">
+        <Navbar />
+       {this.props.children}
+      </div>
     );
   },
 });
